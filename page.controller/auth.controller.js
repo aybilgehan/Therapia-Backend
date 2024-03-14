94ce40019558aef248ab33b9f78b9272d430058e
aybilgehan <bilgehanay@outlook.com>
2024-03-14 23:49:36 +0300

    mail service live

--- page.controller/auth.controller.js
+++ page.controller/auth.controller.js
@@ -1,18 +1,75 @@
 require('dotenv').config();
 const Users = require('../db.handler/user.model');
+const transporter = require('../services/mail.service');
+const uuid = require('uuid');
 
 var jwt = require('jsonwebtoken');
+
+var temp_users = [];
+
+
+setInterval(() => {
+    temp_users = temp_users.filter(u => u.date > Date.now() - 5 * 60 * 1000);
+}, 1000 * 60 * 3);
+
+
+exports.verify = async (req, res) => {
+    try {
+        let user = temp_users.find(u => u.code === req.params["code"] && u.date > Date.now() - 5 * 60 * 1000);
+        if (user) {
+            const index = temp_users.indexOf(user);
+            if (index !== -1) {
+                temp_users.splice(index, 1);
+            }
+            await Users.create({
+                email: user.email,
+                password: user.password
+            });
+            res.status(200).send({ message: "User created" });
+            return;
+        } else {
+            res.status(400).send({ message: "Invalid Verification" });
+            return;
+        }
+    } catch (error) {
+        res.status(500).send({ message: error });
+        return;
+    }
+}
+
 
 exports.signup = async (req, res) => {
     try {
         let checkEmail = await Users.findOne({ "email": req.body.email })
 
+        if (temp_users.find(u => u.email === req.body.email)) {
+            res.status(400).send({ message: "Verification email already sent" });
+            return;
+        }
+
         if (checkEmail) {
             res.status(400).send({ message: "Email already exists" });
             return;
         } else {
-            await Users.create(req.body);
-            res.status(200).send({ message: "User created" });
+            let code = uuid.v4();
+            await transporter.sendMail({
+                from: process.env.EMAIL,
+                to: req.body.email,
+                subject: "Therapia Account Verification",
+                text: `Please click the link to verify your account: ${process.env.APP_URL}/api/auth/verify/${code}`
+            }, (error, info) => {
+                if (error) {
+                    res.status(500).send({ message: error });
+                } else {
+                    temp_users.push({
+                        email: req.body.email,
+                        password: req.body.password,
+                        code: code,
+                        date: Date.now()
+                    });
+                    res.status(200).send({ message: "Verification email sent" });
+                }
+            });
             return;
         }
     } catch (error) {
@@ -29,7 +86,7 @@ exports.signin = async (req, res) => {
             return;
         } else {
             const token = await jwt.sign(
-                { userId: user._id }, 
+                { userId: user._id },
                 process.env.JWT_SECRET,
                 {
                     algorithm: "HS256",
@@ -37,10 +94,10 @@ exports.signin = async (req, res) => {
                     expiresIn: 86400,
                 });
             req.session.token = token;
-            res.status(200).send({ 
+            res.status(200).send({
                 user: user.information ? user.information : user.email,
                 role: user.role
-             });
+            });
             return;
         }
     } catch (error) {
--- routes/page.router.js
+++ routes/page.router.js
@@ -6,12 +6,11 @@ const authController = require("../page.controller/auth.controller.js");
 const mw = require("../middlewares/mw.js");
 
 
-
-
 /* - WEBSITE POST ISLEMLERI - */
 router.post("/api/auth/signin", mw.checkUserNotLoggedIn, authController.signin); 
 router.post("/api/auth/signup", mw.checkUserNotLoggedIn, authController.signup);
 router.post("/api/auth/logout", mw.checkUserLoggedIn, authController.logout);
+router.get("/api/auth/verify/:code", mw.checkUserNotLoggedIn, authController.verify);
 
 /* - TEST ISLEMLERI - */
 
