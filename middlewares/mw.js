8e8936ca1cef8e8c57a9fb01f3e02dddf2c8a4f3
Bilgehan AY <bilgehanay@std.iyte.edu.tr>
2024-05-13 23:22:15 +0300

    hot fix

--- middlewares/mw.js
+++ middlewares/mw.js
@@ -12,7 +12,7 @@ exports.checkUserLoggedIn = async (req, res, next) => {
 }
 
 exports.checkUserNotLoggedIn = async (req, res, next) => {
-    if (!req.session.token) {
+    if (req.session.token) {
         res.status(401).send({ message: "Unauthorized" });
         return;
     } else {
